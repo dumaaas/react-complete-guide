3dd6941aff4e00cbfceb57df6b0bfdd4c75e5122
dumaaas <markodumnic8@gmail.com>
2021-03-09 21:45:50 +0100

    First ever react component

---
+++ 1_understanding-the-base-features-and-syntax/src/App.js
@@ -0,0 +1,17 @@
+import "./App.css";
+import Person from './Person/Person';
+
+function App() {
+  return (
+    <div className="App">
+      <h1> Hi, I 'm a React App </h1>
+      <p> This is really working </p>
+      <Person/>
+      <Person/>
+      <Person/>
+
+    </div>
+  );
+}
+
+export default App;
---
+++ 1_understanding-the-base-features-and-syntax/src/Person/Person.js
@@ -0,0 +1,9 @@
+
+
+function Person() {
+  return (
+      <p> I'm a Person and I am {Math.floor(Math.random() * 30)} yeards old!</p>
+  );
+}
+
+export default Person;
