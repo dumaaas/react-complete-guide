289acab018ef40654619ffac06ec94411ba1ebcb
dumaaas <markodumnic8@gmail.com>
2021-03-12 20:33:27 +0100

    Working with inline styles

--- 1_understanding-the-base-features-and-syntax/src/App.js
+++ 1_understanding-the-base-features-and-syntax/src/App.js
@@ -36,12 +36,25 @@ class App extends Component {
   }
 
   render() {
+
+    const style = {
+      backgroundColor: 'white',
+      font: 'inherit',
+      border: '1px solid blue',
+      padding: '8px',
+      cursor: 'pointer',
+    };
+
     return (
       <div className="App">
         <h1> Hi, I 'm a React App </h1>
         <p> This is really working </p>
         {/* One way to bind value and execute function on click  */}
-        <button onClick={() => this.switchNameHandler('Markisa')}>Switch name</button> 
+        <button 
+          onClick={() => this.switchNameHandler('Markisa')}
+          style={style}>
+            Switch name
+        </button> 
         <Person
           name={this.state.persons[0].name}
           age={this.state.persons[0].age}
