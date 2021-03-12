7fd83a6975b6279688e20ea78c3f44c05d33322f
dumaaas <markodumnic8@gmail.com>
2021-03-12 20:13:55 +0100

    Passing method references between components

--- 1_understanding-the-base-features-and-syntax/src/App.js
+++ 1_understanding-the-base-features-and-syntax/src/App.js
@@ -1,103 +1,111 @@
-import React, {useState} from 'react';
+import React, {Component} from 'react';
 import "./App.css";
 import Person from "./Person/Person";
 
-
-//FUNCTIONAL BASED COMPONENTS
-const App = props => {
-
-  const [ personsState, setPersonsState] = useState ({
+// CLASS BASED COMPONENTS
+class App extends Component {
+  state = {
     persons: [
       { name: "Marko", age: 28 },
       { name: "Stefan", age: 26 },
       { name: "Bojan", age: 30 },
-    ],
-    otherState: 'some other value'
-  });
-  
+    ]
+  }
 
-  const switchNameHandler = () => {
+  switchNameHandler = (newName) => {
+    // Wrong way!
+    // this.state.persons[0].name = 'Markisa';
+    // Right way!
+    this.setState({
+      persons: [
+        { name: newName, age: 28 },
+        { name: "Stefan", age: 26 },
+        { name: "Vujanac", age: 30 },
+      ]
+    })
+  }
 
-    setPersonsState({
-        persons: [
-          { name: "Markisa", age: 25 },
-          { name: "Stefan", age: 41 },
-          { name: "Vujanac", age: 30 },
-        ],
-        otherState: personsState.otherState
-      });
-    };
-  
- 
-    
+  render() {
     return (
       <div className="App">
         <h1> Hi, I 'm a React App </h1>
         <p> This is really working </p>
-        <button onClick={switchNameHandler}>Switch name</button>
+        {/* One way to bind value and execute function on click  */}
+        <button onClick={() => this.switchNameHandler('Markisa')}>Switch name</button> 
         <Person
-          name={personsState.persons[0].name}
-          age={personsState.persons[0].age}
+          name={this.state.persons[0].name}
+          age={this.state.persons[0].age}
         />
         <Person
-          name={personsState.persons[1].name}
-          age={personsState.persons[1].age}
-        />
+          name={this.state.persons[1].name}
+          age={this.state.persons[1].age}
+          /* 
+          Another way to bind value and execute function on click  
+          Its better to use this way whenever we can!
+          */
+          click={this.switchNameHandler.bind(this, 'Dumaraaa!')}
+        >
+          My Hobbies: Racing
+        </Person>
         <Person
-          name={personsState.persons[2].name}
-          age={personsState.persons[2].age}
+          name={this.state.persons[2].name}
+          age={this.state.persons[2].age}
         />
       </div>
     );
-
+  }
 }
 
 export default App;
 
+//FUNCTIONAL BASED COMPONENTS
+// const App = props => {
 
-//CLASS BASED COMPONENTS
-
-// class App extends Component {
-//   state = {
+//   const [ personsState, setPersonsState] = useState ({
 //     persons: [
 //       { name: "Marko", age: 28 },
 //       { name: "Stefan", age: 26 },
 //       { name: "Bojan", age: 30 },
-//     ]
-//   }
+//     ],
+//     otherState: 'some other value'
+//   });
+  
 
-//   switchNameHandler = () => {
-//     Wrong way!
-//     this.state.persons[0].name = 'Markisa';
-//     Right way!
-//     this.setState({
-//       persons: [
-//         { name: "Markisa", age: 28 },
-//         { name: "Stefan", age: 26 },
-//         { name: "Vujanac", age: 30 },
-//       ]
-//     })
-//   }
+//   const switchNameHandler = () => {
 
-//   render() {
+//     setPersonsState({
+//         persons: [
+//           { name: "Markisa", age: 25 },
+//           { name: "Stefan", age: 41 },
+//           { name: "Vujanac", age: 30 },
+//         ],
+//         otherState: personsState.otherState
+//       });
+//     };
+  
+ 
+    
 //     return (
 //       <div className="App">
 //         <h1> Hi, I 'm a React App </h1>
 //         <p> This is really working </p>
-//         <button onClick={this.switchNameHandler}>Switch name</button>
+//         <button onClick={switchNameHandler}>Switch name</button>
 //         <Person
-//           name={this.state.persons[0].name}
-//           age={this.state.persons[0].age}
+//           name={personsState.persons[0].name}
+//           age={personsState.persons[0].age}
+//           click={this.switchNameHandler}
 //         />
 //         <Person
-//           name={this.state.persons[1].name}
-//           age={this.state.persons[1].age}
+//           name={personsState.persons[1].name}
+//           age={personsState.persons[1].age}
 //         />
 //         <Person
-//           name={this.state.persons[2].name}
-//           age={this.state.persons[2].age}
+//           name={personsState.persons[2].name}
+//           age={personsState.persons[2].age}
 //         />
 //       </div>
 //     );
-//   }
 // }
+
+
+
--- 1_understanding-the-base-features-and-syntax/src/Person/Person.js
+++ 1_understanding-the-base-features-and-syntax/src/Person/Person.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Person = (props) => {
   return (
     <div>
-      <p>I'm {props.name} and I am {props.age} years old!</p>
+      <p onClick={props.click}>I'm {props.name} and I am {props.age} years old!</p>
       <p>{props.children}</p>
     </div>
   )
