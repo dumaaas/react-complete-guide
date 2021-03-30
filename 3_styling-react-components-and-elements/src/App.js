ab3aa31b33a9c9a01cd4359a5cf54046a2e901a0
dumaaas <markodumnic8@gmail.com>
2021-03-30 15:31:38 +0200

    Preparing to run eject script

--- 3_styling-react-components-and-elements/src/App.js
+++ 3_styling-react-components-and-elements/src/App.js
@@ -1,6 +1,5 @@
 import React, {Component} from 'react';
 import "./App.css";
-import Radium, {StyleRoot} from 'radium';
 import Person from "./Person/Person";
 
 // CLASS BASED COMPONENTS
@@ -77,10 +76,6 @@ class App extends Component {
       border: '1px solid blue',
       padding: '8px',
       cursor: 'pointer',
-      ':hover': {
-        backgroundColor: 'lightgreen',
-        color: 'black'
-      }
     };
 
     let persons = null;
@@ -100,10 +95,6 @@ class App extends Component {
         </div>
       );
       style.backgroundColor = 'red';
-      style[':hover'] = {
-        backgroundColor: 'salmon',
-        color: 'black'
-      }
     }
 
     //Setting class names dynamically 
@@ -116,25 +107,23 @@ class App extends Component {
     }
 
     return (
-      <StyleRoot>
-        <div className="App">
-          <h1> Hi, I 'm a React App </h1>
-          <p className={classes.join(' ')}> This is really working </p>
-          {/* One way to bind value and execute function on click  */}
-          <button 
-            onClick={this.togglePersonsHandler}
-            style={style}>
-              Show Persons
-          </button> 
-          {persons}
-        </div>
-      </StyleRoot>
+      <div className="App">
+        <h1> Hi, I 'm a React App </h1>
+        <p className={classes.join(' ')}> This is really working </p>
+        {/* One way to bind value and execute function on click  */}
+        <button 
+          onClick={this.togglePersonsHandler}
+          style={style}>
+            Show Persons
+        </button> 
+        {persons}
+      </div>
     );
 
   }
 }
 
-export default Radium(App);
+export default App
 
 
 
--- 3_styling-react-components-and-elements/src/Person/Person.js
+++ 3_styling-react-components-and-elements/src/Person/Person.js
@@ -1,15 +1,9 @@
 import React from 'react';
-import Radium from 'radium';
 import './Person.css';
 
 const Person = (props) => {
-  const style = {
-    '@media (min-widht: 500px)': {
-      width: '450px'
-    }
-  };
   return (
-    <div className="Person" style={style}>
+    <div className="Person">
       <p onClick={props.click}>I'm {props.name} and I am {props.age} years old!</p>
       <p>{props.children}</p>
       <input type="text" onChange={props.changed} value={props.name} />
@@ -17,13 +11,23 @@ const Person = (props) => {
   )
 }
 
-// function Person(props) {
+export default Person;
+
+
+// RADIUM LESSON -> USING WITH MEDIA QUERIES
+// const Person = (props) => {
+//   const style = {
+//     '@media (min-widht: 500px)': {
+//       width: '450px'
+//     }
+//   };
 //   return (
-//     <div>
-//       <p> I'm a {props.name} and I am {props.age} yeards old!</p>
+//     <div className="Person" style={style}>
+//       <p onClick={props.click}>I'm {props.name} and I am {props.age} years old!</p>
 //       <p>{props.children}</p>
+//       <input type="text" onChange={props.changed} value={props.name} />
 //     </div>
-//   );
+//   )
 // }
 
-export default Radium(Person);
+// export default Radium(Person);
